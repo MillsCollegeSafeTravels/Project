80adf9e5a668eea3a1deeb700f49ac93520a0181
kfeeney <feeney.kate@gmail.com>
2011-03-24 15:31:19 -0700

    Attempting to get the locator working, it crashes when the LocationFinder class loads

--- gen/edu/mills/cs180/safetravels/R.java
+++ gen/edu/mills/cs180/safetravels/R.java
@@ -16,17 +16,15 @@ public final class R {
         public static final int main_name=0x7f020002;
     }
     public static final class id {
-        public static final int logo_view=0x7f050000;
-        public static final int made_it_button=0x7f050005;
-        public static final int mapview=0x7f050003;
-        public static final int route_button=0x7f050002;
-        public static final int send_to_friend_button=0x7f050004;
-        public static final int track_button=0x7f050001;
+        public static final int logo_view=0x7f050001;
+        public static final int output=0x7f050000;
+        public static final int route_button=0x7f050003;
+        public static final int track_button=0x7f050002;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int routepage=0x7f030001;
-        public static final int trackpage=0x7f030002;
+        public static final int locationfinder=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int routepage=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
